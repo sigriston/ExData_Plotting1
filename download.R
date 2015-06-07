443988da31c0a744cc1b3f0888100d98e65309db
Thiago Sigrist <sigrist@gmail.com>
2015-06-07 15:47:29 -0300

    remove aux files

--- download.R
+++
@@ -1,5 +0,0 @@
-library(downloader)
-
-download("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
-         "household_power_consumption.zip")
-unzip("household_power_consumption.zip", exdir = "data")
